5184034a2e02c73c15c75d062b07490fb12f96c5
Baron Chandler <bchandler@rentpath.com>
2016-09-12 23:20:00 -0400

    add caching

---
+++ manifests/addCache.pp
@@ -0,0 +1,36 @@
+# Define: dnsdist::addcache
+#
+#  adds a cache to a pool
+#
+# Author
+#   Baron Chandler <baron@baronchandler.com>
+#
+# Version
+#   0.1   Initial release
+#
+# Parameters (see cachine docs at dnsdist.org for details):
+#   $pool (defaults to global)
+#   $size (defaults to 10,000 entries)
+#   $max_life (1 day)
+#   $min_ttl_to_cache (0 seconds)
+#   $srv_fail_ttl (60 seconds)
+#   $stale_ttl (60 seconds)
+#
+# Requires:
+#
+# Sample Usage:
+#
+#   dnsdist::addcache { 'setup global cache': }
+#
+#   dnsdist::addcache { 'add abuse pool cache':
+#               pool => 'abuse', }
+#
+
+define dnsdist::addcache ($pool='', $varname='', $size=10000, $max_life=86400, $min_ttl_to_cache=0, $srv_fail_ttl, $stale_ttl) {
+  concat::fragment { "addcache-${order}-${title}":
+    target  => "/etc/dnsdist/dnsdist.conf",
+    content => "pc_${order} = newPacketCache(${size}, ${max_life}, ${min_ttl_to_cache}, ${srv_fail_ttl}, ${$stale_ttl}\
+        getPool(\"${pool}\"):setCache(pc_${order})\n"
+    order   => "60"
+  }
+}
